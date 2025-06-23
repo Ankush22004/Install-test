b6c12f14fd3ae4829f4ff85cfcccecb191bbbd9b
Ankush22004 <akushS0192@gmail.com>
2025-06-23 07:38:41 +0530

    Create install.js

---
+++ install.js
@@ -0,0 +1,21 @@
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  e.preventDefault();
+  deferredPrompt = e;
+  document.getElementById('installBtn').style.display = 'inline-block';
+});
+
+function installApp() {
+  if (deferredPrompt) {
+    deferredPrompt.prompt();
+    deferredPrompt.userChoice.then(choiceResult => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('✅ App installed');
+      } else {
+        console.log('❌ App installation rejected');
+      }
+      deferredPrompt = null;
+    });
+  }
+}
